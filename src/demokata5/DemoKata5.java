a1d50f8bdce49a0f0c15926efebea0fbf95fdc97
Entrar <Entrar@sopa6>
2021-11-10 09:28:49 +0000

    Ejemplo 4, DemoKata5

--- src/demokata5/DemoKata5.java
+++ src/demokata5/DemoKata5.java
@@ -1,9 +1,10 @@
 package demokata5;
 
 import static demokata5.CrearBD.createNewDatabase;
+import static demokata5.CrearTabla.createNewTable;
 
 public class DemoKata5 {
     public static void main(String[] args) {
-        createNewDatabase("mail.db");
+        createNewTable();
     }  
 }
